77c7ce3343a62a997948d223bd5454cbed10f389
Misha Serbenyuk <outsourcing.you@gmail.com>
2023-06-14 15:39:19 +0300

    Fix envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-repositoryName=$1
-imageTagMutability=$2
-scanOnPush=$3
-tags=$4
-tagStatus=$5
-countType=$6
-countNumber=$7
+repositoryName=$INPUT_REPOSITORYNAME
+imageTagMutability=$INPUT_IMAGETAGMUTABILITY
+scanOnPush=$INPUT_SCANONPUSH
+tags=$INPUT_TAGS
+tagStatus=$INPUT_TAGSTATUS
+countType=$INPUT_COUNTTYPE
+countNumber=$INPUT_COUNTNUMBER
 
 python /main.py
